6d517dd75f3e140bd1d5ece5bda37b06e663c825
mukeshBagal <2022bit039@sggs.ac.in>
2024-03-04 20:15:44 +0530

    Sum Of Two Number

---
+++ Sum_Of_Two_Number.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num1,num2,sum;
+	cout<<"Enter 1st Number: ";
+	cin>>num1;
+	cout<<"Enter 2nd Number: ";
+	cin>>num2;
+	sum=num1+num2;
+	cout<<"Sum Of Two Number: "<<sum<<endl;
+	
+	return 0;
+}
+
+/*
+OUTPUT::
+
+	Enter 1st Number: 6
+	Enter 2nd Number: 5
+	Sum Of Two Number: 11
+
+*/
