95202f264cac0c318d26295f7f1fef054524639c
mukeshBagal <2022bit039@sggs.ac.in>
2024-03-07 20:00:30 +0530

    Program of Increment and Decrement Operator

---
+++ Icrement_Decrement_Operator.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int x,y;
+	x=10;
+	y=12;
+	x++;
+	y--;
+	cout<<"After Increment the number(Used Increment Operator): "<<x<<endl;
+	cout<<"After Decrement the number(Used Decrment Operator): "<<y<<endl;
+	
+	return 0;
+}
+
+/*
+OUTPUT::
+
+	After Increment the number(Used Increment Operator): 11
+	After Decrement the number(Used Decrment Operator): 11
+
+*/
