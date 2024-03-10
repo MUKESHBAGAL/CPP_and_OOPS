c1a3459a4b91c4a686839a06799454dc0fa2d0d9
mukeshBagal <2022bit039@sggs.ac.in>
2024-03-10 11:33:31 +0530

    INDEX OF PROGRAM

--- 01.INDEX.cpp
+++ 01.INDEX.cpp
@@ -1,1 +1,13 @@
-dd
+                                                                                          **INDEX_OF_PROGRAM**
+# INTRODUCTION
+	1.sum of two number
+
+# OPERATOR
+	1.Assignment Operator
+	2.Arithmetic Operator
+	3.Increment And Decrement Operator
+	4.Relational Operator
+	5.Logical Operator
+	6.Conditional Operator(In conditional Operator program we find largest number)
+	7.
+
